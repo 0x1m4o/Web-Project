8c78bb0cfcf1949209a232646043de0dbb668b85
IlhamSaputra10 <ilham.69cand@gmail.com>
2022-12-19 15:54:01 +0700

    Maritory App

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{$title}} | Maritory</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    {{-- Font Google Poppins --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+
+    {{-- CDN Boostrap Icon --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css" integrity="sha384-b6lVK+yci+bfDmaY1u0zE8YYJt0TZxLEAFyYSLHId4xoVvsrQu3INevFKo+Xir8e" crossorigin="anonymous">
+
+    {{-- CND Jquery --}}
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+    {{-- CSS --}}
+    <link rel="stylesheet" href="/css/index.css">
+</head>
+  <body>
+    @include('partials.navbar')
+    @yield('content')
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,44 @@
+<nav class="navbar navbar-expand-lg" style="background: #EDEEF7;font-size: 13px;">
+    <div class="container-fluid">
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">Feed</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Checklist</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Rab</a>
+          </li>
+          <li class="nav-item" style="margin-left: 900px;">
+            <a class="nav-link">Mulai Jualan</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link">Mitra Maritory</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link">Tentang Maritory</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+<nav class="navbar navbar-expand-lg" style="background: #1E6F5C">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="/img/logos.png" alt="Logo" width="150" height="55" class="d-inline-block align-text-top">
+          </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="container" id="navbarSupportedContent">
+        <form class="d-flex m-auto" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn text-light" style="background: #FDB662;" type="submit"><i class="bi bi-search"></i></button>
+        </form>
+      </div>
+      <button class="btn btn-light mx-3" style="color: #FDB662;background: white;">Masuk</button>
+      <button class="btn" style="background: #FDB662;color: white;">Daftar</button>
+    </div>
+  </nav>
