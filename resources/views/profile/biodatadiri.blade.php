2adc1a8e9a5fe853e478338c211da919dae66b68
ridwanalfarezi <alfareziridwan@gmail.com>
2022-12-27 17:17:30 +0700

    membuat biodata diri

--- resources/views/profile/biodatadiri.blade.php
+++ resources/views/profile/biodatadiri.blade.php
@@ -204,8 +204,96 @@
                           <a class="navbar-brand" href="/daftarrekening">Daftar Rekening</a>
                           <a class="navbar-brand" href="/keamanan">Keamanan</a>
                         </div>
-                      </nav>
-                Biodata Diri
+                    </nav>
+                    <div class="row p-5">
+                        <div class="col-4">
+                            <div class="card" style="box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);">
+                                <img src="/img/Rectangle 35.png" class="card-img-top px-5 py-4">
+                                <div class="card-body text-center mb-3">
+                                   <button class="btn btn-outline-dark">Pilih Foto</button> 
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-8">
+                          <div class="container-fluid" style="border-bottom: 1px solid grey;">
+                            <div class="row align-items-center">
+                                <div class="col-11">
+                                    <span class="mb-3 fs-4">
+                                        Biodata Diri
+                                    </span>
+                                </div>
+                                <div class="col-1">
+                                        <i class="bi bi-pencil-fill fs-4"></i>
+                                </div>
+                            </div>
+                            <div class="col-sm-2" style="cursor: pointer; color: black;">
+                            </div>
+                            <div class="row ms-2 my-3">
+                                <div class="col-6">
+                                    <p class="mb-4">Nama</p>
+                                    <p class="mb-4">Tanggal Lahir</p>
+                                    <p class="mb-4">Jenis Kelamin</p>
+                                </div>
+                                <div class="col-6">
+                                    <p class="mb-4">Vanessa Oey</p>
+                                    <p class="mb-4">30 Desember 1993</p>
+                                    <p class="mb-4">Wanita</p>
+                                </div>
+                            </div>
+                          </div>  
+
+                          <div class="container-fluid mt-3" style="border-bottom: 1px solid grey;">
+                            <div class="row align-items-center">
+                                <div class="col-11">
+                                    <span class="mb-3 fs-4">
+                                        Kontak
+                                    </span>
+                                </div>
+                                <div class="col-1">
+                                        <i class="bi bi-pencil-fill fs-4"></i>
+                                </div>
+                            </div>
+                            <div class="col-sm-2" style="cursor: pointer; color: black;">
+                            </div>
+                            <div class="row ms-2 my-3">
+                                <div class="col-6">
+                                    <p class="mb-4">Nama</p>
+                                    <p class="mb-4">Nomor HP</p>
+                                </div>
+                                <div class="col-6">
+                                    <p class="mb-4">vanessa.oey@gmail.com</p>
+                                    <p class="mb-4">087804842080</p>
+                                </div>
+                            </div>
+                          </div>
+                            
+                          <div class="container-fluid mt-3">
+                            <div class="row align-items-center">
+                                <div class="col-11">
+                                    <span class="mb-3 fs-4">
+                                        Sandi
+                                    </span>
+                                </div>
+                                <div class="col-1">
+                                        <i class="bi bi-pencil-fill fs-4"></i>
+                                </div>
+                            </div>
+                            <div class="col-sm-2" style="cursor: pointer; color: black;">
+                            </div>
+                            <div class="row ms-2 my-3">
+                                <div class="col-6">
+                                    <p class="mb-4">Sandi</p>
+                                    <p class="mb-4">Terakhir diubah</p >
+                                </div>
+                                <div class="col-6">
+                                    <p class="mb-4">*************</p>
+                                    <p class="mb-4">18 Januari 2021</p>
+                                </div>
+                            </div>
+                          </div>
+                        </div>
+                    </div>
+                    <hr>
                 </div>
             </div>
           </div>
