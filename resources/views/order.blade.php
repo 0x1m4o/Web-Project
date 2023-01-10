c5da266226d40d315d2bdb716dd4e53dbedd9cb9
0x1m4o <chuntdevnx@gmail.com>
2023-01-10 09:29:04 +0700

    Daftar Vendor 1

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -12,9 +12,12 @@
                     <i class='bx bx-chevron-left' style="padding-top: 25px;font-size: 30px;"></i>
                 </a>
                 <div style="width: 1200px;">
-                <a href="">
-                    <h5 style="padding-top: 20px;font-weight: bold;font-size: 32px; color: #49516F;">Konfirmasi Pesanan</h5>
+                    <a href="" class="text-decoration-none">
+                        <h5 style="padding-top: 20px;font-weight: bold;font-size: 32px; color: #49516F;">Konfirmasi Pesanan
+                        </h5>
+                    </a>
                 </div>
+
             </div>
         </div>
         <hr class="my-0">
